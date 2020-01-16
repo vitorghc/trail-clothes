a1ce482e1da86164a5848f2fc6b916c53c213d66
Vitor Feltrin <vitor.feltrin@sensedia.com>
2020-01-16 14:59:12 -0300

    commit inicial

---
+++ src/main/java/com/trail/clothes/api/TrailClothesApplication.java
@@ -0,0 +1,13 @@
+package com.trail.clothes.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrailClothesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrailClothesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trail/clothes/api/TrailClothesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trail.clothes.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrailClothesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
